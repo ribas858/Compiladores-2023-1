65b42013f352a690f8aa9001e831e14ace7f210f
ribas858 <ribas858@gmail.com>
2023-04-25 19:45:16 -0300

    numero negativo/positivo e identificador

--- Analisador Léxico/lex.yy.c
+++ Analisador Léxico/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[8] =
+static const flex_int16_t yy_accept[15] =
     {   0,
-        0,    0,    5,    2,    3,    1,    0
+        0,    0,    6,    3,    4,    3,    1,    2,    3,    3,
+        1,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -371,16 +372,16 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
+        1,    1,    1,    1,    3,    1,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
+        1,    1,    1,    1,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    5,    1,    5,    5,    5,    5,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    1,    1
+        1,    2,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[8] =
+static const flex_int16_t yy_base[16] =
     {   0,
-        0,    0,    4,    5,    5,    5,    5
+        0,    0,   12,    0,   13,    3,    7,    4,    0,    0,
+        0,    0,    0,   13,    9
     } ;
 
-static const flex_int16_t yy_def[8] =
+static const flex_int16_t yy_def[16] =
     {   0,
-        7,    1,    7,    7,    7,    7,    0
+       14,    1,   14,   15,   14,   15,   15,   15,   15,    6,
+        7,    7,    8,    0,   14
     } ;
 
-static const flex_int16_t yy_nxt[9] =
+static const flex_int16_t yy_nxt[19] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7,    7
+        4,    5,    6,    7,    8,   10,   11,   13,   13,    9,
+       12,   14,    3,   14,   14,   14,   14,   14
     } ;
 
-static const flex_int16_t yy_chk[9] =
+static const flex_int16_t yy_chk[19] =
     {   0,
-        1,    1,    1,    3,    7,    7,    7,    7
+        1,    1,    1,    1,    1,    6,    6,    8,    8,   15,
+        7,    3,   14,   14,   14,   14,   14,   14
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,12 +441,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "flex_teste.l"
-#line 2 "flex_teste.l"
+#line 1 "analisador_lex_Cminus_base.l"
+#line 2 "analisador_lex_Cminus_base.l"
     /* definições */
 
-#line 444 "lex.yy.c"
-#line 445 "lex.yy.c"
+#line 449 "lex.yy.c"
+#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -658,10 +663,9 @@ YY_DECL
 		}
 
 	{
-#line 6 "flex_teste.l"
+#line 6 "analisador_lex_Cminus_base.l"
 
-
-#line 665 "lex.yy.c"
+#line 669 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -688,13 +692,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 15 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -720,26 +724,37 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "flex_teste.l"
-{printf("Minusculas\n");}
+#line 7 "analisador_lex_Cminus_base.l"
+{
+        if (yytext[0] == '-') {
+            printf("\nNúmero Negativo\n\n");
+        } else {
+            printf("\nNumero Positivo\n\n");
+        }
+    }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 10 "flex_teste.l"
-{printf("erro\n");}
+#line 15 "analisador_lex_Cminus_base.l"
+{printf("\nIdentificador (Variável)\n\n");}
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 12 "flex_teste.l"
-{return 0;}
+#line 17 "analisador_lex_Cminus_base.l"
+{printf("erro\n");}
 	YY_BREAK
 case 4:
+/* rule 4 can match eol */
 YY_RULE_SETUP
-#line 14 "flex_teste.l"
+#line 19 "analisador_lex_Cminus_base.l"
+{return 0;}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 21 "analisador_lex_Cminus_base.l"
 ECHO;
 	YY_BREAK
-#line 743 "lex.yy.c"
+#line 758 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1036,7 +1051,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 15 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1064,11 +1079,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 15 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 14);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1744,14 +1759,14 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 14 "flex_teste.l"
+#line 21 "analisador_lex_Cminus_base.l"
 
 
 int yywrap() {}
 
 
 int main() {
-    printf("Enter a string");
+    printf("\nOlá, digite algo e o classificaremos para você.\n>> ");
     yylex();
 
     return 0;
